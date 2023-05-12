8c83736c3f22a3cc09981badf6616c842e0d9b87
nikhil <nikhil.sontakke19@vit.edu>
2023-05-13 00:49:36 +0530

    Added Cross Origin on all controllers

--- src/main/java/com/example/MobilePass/controller/AccessDoorController.java
+++ src/main/java/com/example/MobilePass/controller/AccessDoorController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/accessdoors")
 public class AccessDoorController {
     @Autowired
--- src/main/java/com/example/MobilePass/controller/ActivityLogController.java
+++ src/main/java/com/example/MobilePass/controller/ActivityLogController.java
@@ -3,13 +3,13 @@ package com.example.MobilePass.controller;
 import com.example.MobilePass.model.ActivityLog;
 import com.example.MobilePass.repository.ActivityLogRepository;
 import com.example.MobilePass.services.ActivityLogService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/activitylogs")
 public class ActivityLogController {
     @Autowired
