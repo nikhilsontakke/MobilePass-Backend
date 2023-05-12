ab0957ecccf7a2bbab491837749cc976df6c8c82
nikhil <nikhil.sontakke19@vit.edu>
2023-05-13 00:18:21 +0530

    Added ActivityLogController

--- src/main/java/com/example/MobilePass/controller/ActivityLogController.java
+++ src/main/java/com/example/MobilePass/controller/ActivityLogController.java
@@ -1,22 +1,31 @@
 package com.example.MobilePass.controller;
 
+import com.example.MobilePass.model.ActivityLog;
+import com.example.MobilePass.repository.ActivityLogRepository;
 import com.example.MobilePass.services.ActivityLogService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/activitylogs")
 public class ActivityLogController {
     @Autowired
     ActivityLogService activityLogService;
+    @Autowired
+    ActivityLogRepository activityLogRepository;
 
     @GetMapping
     public ResponseEntity<Object> getAllActivities(){
         return new ResponseEntity<>(activityLogService.getAllRecords(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveActivity(@RequestBody ActivityLog activityLog){
+        activityLogRepository.save(activityLog);
+        return new ResponseEntity<>("Activity Logged Successfully", HttpStatus.OK);
     }
 
 }
